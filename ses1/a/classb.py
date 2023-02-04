72b8abba3021cb8bdb847006117de54a5a7bcde9
clbokea <clbo@kea.dk>
2023-02-04 13:54:35 +0100

    initial commit

---
+++ ses1/a/classb.py
@@ -0,0 +1,20 @@
+import sys
+
+def message(x, y):
+    if x == 'Claus':
+        print(f'Hello {x}')
+    if y == 'Anna':
+        print(y)
+    else:
+        prittt('NO NO NO')
+
+message(sys.argv, sys.argv[2])
+
+
+
+
+
+
+
+
+
---
+++ ses1/a/script.py
@@ -0,0 +1,15 @@
+
+import sys
+
+def message(x):
+    if x[1] == 'Claus':
+        print('Hello ' + x[1])
+    elif x[1] == 'Anna':
+        hfdaldkfhakjfs
+    else:
+        print('This is not wrong code')
+    
+
+message(sys.argv)
+
+
